b7c5fd7fa9508a8e02d8fbb2304511ca51f79a15
Hugh Barnes <dev@hubns.com>
2023-02-09 19:06:58 +1300

    Allow setting parent theme (Quark) settings via Admin even though it's not active

--- modules.php
+++ modules.php
@@ -2,8 +2,31 @@
 namespace Grav\Theme;
 
 use Grav\Common\Theme;
+use RocketTheme\Toolbox\Event\Event;
 
 class Modules extends Quark
 {
-    // Access plugin events in this class
+    public static function getSubscribedEvents() {
+        return [
+            'onAdminTwigTemplatePaths' => ['onAdminTwigTemplatePaths', 0],
+        ];
+    }
+
+    // sourced from https://discourse.getgrav.org/t/customize-override-admin-theme/6371/9
+    /**
+     * Register templates and page
+     *
+     * @param RocketTheme\Toolbox\Event\Event $event Event handler
+     *
+     * @return array
+     */
+    public function onAdminTwigTemplatePaths($event)
+    {
+        $event['paths'] = array_merge(
+            $event['paths'],
+            [__DIR__ . '/admin/themes/grav/templates']
+        );
+        return $event;
+    }
+
 }
