48ed4fd0c406b2e2b98ddddc06519b5b4c8f5844
Hugh Barnes <dev@hubns.com>
2023-04-11 13:36:44 +1200

    Add parent theme Twig alias; add 'images' alias to Twig because the one in Quark fails for child themes

--- modules.php
+++ modules.php
@@ -1,6 +1,7 @@
 <?php
 namespace Grav\Theme;
 
+use Grav\Common\Grav;
 use Grav\Common\Theme;
 use RocketTheme\Toolbox\Event\Event;
 
@@ -8,8 +9,29 @@ class Modules extends Quark
 {
     public static function getSubscribedEvents() {
         return [
+            'onTwigLoader' => ['onTwigLoader', 10],
             'onAdminTwigTemplatePaths' => ['onAdminTwigTemplatePaths', 0],
         ];
+    }
+
+    public function onTwigLoader() {
+        $this->parentHandler('onTwigLoader');
+
+        // add quark theme as namespace to twig
+        $parent_path = Grav::instance()['locator']->findResource('themes://quark');
+        $this->grav['twig']->addPath($parent_path . DIRECTORY_SEPARATOR . 'templates', 'quark');
+        $this->grav['twig']->addPath($parent_path . DIRECTORY_SEPARATOR . 'images', 'images'); // special for Quark's nonsense
+    }
+
+    private function parentHandler($event) {
+        if(method_exists(parent::class, 'getSubscribedEvents')) {
+            $parent_subscribed_events = parent::getSubscribedEvents();
+
+            if(array_key_exists($event, $parent_subscribed_events)) {
+                $parent_event_handler = array_shift($parent_subscribed_events[$event]);
+                call_user_func([parent::class, $parent_event_handler]);
+            }
+        }
     }
 
     // sourced from https://discourse.getgrav.org/t/customize-override-admin-theme/6371/9
